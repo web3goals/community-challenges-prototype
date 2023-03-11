758739dda656e584734b1a38f313b604eaf87bfe
kiv1n <aloha@kiv1n.ru>
2023-03-11 19:03:13 +0300

    Add 5ire, mantle, xdc chains to app

--- app/utils/chains.ts
+++ app/utils/chains.ts
@@ -9,6 +9,111 @@ const mumbaiProfileContractAddress =
 const mumbaiChallengeContractAddress =
   process.env.NEXT_PUBLIC_MUMBAI_CHALLENGE_CONTRACT_ADDRESS;
 
+const fiveIreTestnetProfileContractAddress =
+  process.env.NEXT_PUBLIC_5IRE_TESTNET_PROFILE_CONTRACT_ADDRESS;
+const fiveIreTestnetChallengeContractAddress =
+  process.env.NEXT_PUBLIC_5IRE_TESTNET_CHALLENGE_CONTRACT_ADDRESS;
+
+const mantleTestnetProfileContractAddress =
+  process.env.NEXT_PUBLIC_MANTLE_TESTNET_PROFILE_CONTRACT_ADDRESS;
+const mantleTestnetChallengeContractAddress =
+  process.env.NEXT_PUBLIC_MANTLE_TESTNET_CHALLENGE_CONTRACT_ADDRESS;
+
+const xdcTestnetProfileContractAddress =
+  process.env.NEXT_PUBLIC_XDC_TESTNET_PROFILE_CONTRACT_ADDRESS;
+const xdcTestnetChallengeContractAddress =
+  process.env.NEXT_PUBLIC_XDC_TESTNET_CHALLENGE_CONTRACT_ADDRESS;
+
+/**
+ * Get 5ire testnet chain.
+ */
+export function getFiveIreTestnetChain(): Chain {
+  return {
+    id: 997,
+    name: "5ire Testnet",
+    network: "5ire-testnet",
+    nativeCurrency: {
+      decimals: 18,
+      name: "5ire",
+      symbol: "5ire",
+    },
+    rpcUrls: {
+      default: {
+        http: ["https://rpc-testnet.5ire.network"],
+      },
+      public: {
+        http: ["https://rpc-testnet.5ire.network"],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: "5ire Testnet Explorer",
+        url: "https://explorer.5ire.network",
+      },
+    },
+  };
+}
+
+/**
+ * Get mantle testnet chain.
+ */
+export function getMantleTestnetChain(): Chain {
+  return {
+    id: 5001,
+    name: "Mantle Testnet",
+    network: "mantle-testnet",
+    nativeCurrency: {
+      decimals: 18,
+      name: "BIT",
+      symbol: "BIT",
+    },
+    rpcUrls: {
+      default: {
+        http: ["https://rpc.testnet.mantle.xyz"],
+      },
+      public: {
+        http: ["https://rpc.testnet.mantle.xyz"],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: "Mantle Testnet Explorer",
+        url: "https://explorer.testnet.mantle.xyz",
+      },
+    },
+  };
+}
+
+/**
+ * Get xdc testnet chain.
+ */
+export function getXdcTestnetChain(): Chain {
+  return {
+    id: 51,
+    name: "XDC Apothem Testnet",
+    network: "xdc-testnet",
+    nativeCurrency: {
+      decimals: 18,
+      name: "XDC",
+      symbol: "XDC",
+    },
+    rpcUrls: {
+      default: {
+        http: ["https://erpc.apothem.network"],
+      },
+      public: {
+        http: ["https://erpc.apothem.network"],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: "XDC Testnet Explorer",
+        url: "https://explorer.apothem.network/",
+      },
+    },
+  };
+}
+
 /**
  * Get the first chain from supported chains.
  */
@@ -28,6 +133,21 @@ export function getSupportedChains(): Array<Chain> {
   const chains: Array<Chain> = [];
   if (mumbaiChallengeContractAddress && mumbaiProfileContractAddress) {
     chains.push(polygonMumbai);
+  }
+  if (
+    fiveIreTestnetChallengeContractAddress &&
+    fiveIreTestnetProfileContractAddress
+  ) {
+    chains.push(getFiveIreTestnetChain());
+  }
+  if (
+    mantleTestnetChallengeContractAddress &&
+    mantleTestnetProfileContractAddress
+  ) {
+    chains.push(getMantleTestnetChain());
+  }
+  if (xdcTestnetChallengeContractAddress && xdcTestnetProfileContractAddress) {
+    chains.push(getXdcTestnetChain());
   }
   if (chains.length === 0) {
     console.error("Not found supported chains");
