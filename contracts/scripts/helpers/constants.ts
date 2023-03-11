6b47f90646ee7a9de78935c0f1a72f691d39b28a
kiv1n <aloha@kiv1n.ru>
2023-03-11 16:17:56 +0300

    Deploy contracts to 5ire, mantle, xdc

--- contracts/scripts/helpers/constants.ts
+++ contracts/scripts/helpers/constants.ts
@@ -12,9 +12,17 @@ export const deployedContracts: {
   },
   "5ireTestnet": {
     profile: "0x96E6AF6E9e400d0Cd6a4045F122df22BCaAAca59",
+    challenge: "0x1e4712A93beEc0aa26151CF44061eE91DD56f921",
+    verifier: "0x7Aba7fF10bC3DB6FA82d6b23f5492A7F274101f2",
   },
   mantleTestnet: {
     profile: "0x7Ef7683492D7a610A2ceaE6743B97788EE657bD5",
+    challenge: "0xFB22c49674E4482C22D4499392a8e2f760D84f8d",
+    verifier: "0x4518BA8A80a1555402A4c75D631c36338b5b58c4",
   },
-  xdcTestnet: {},
+  xdcTestnet: {
+    profile: "0x96E6AF6E9e400d0Cd6a4045F122df22BCaAAca59",
+    challenge: "0xFe0AeD5cBEE89869FF505e10A5eBb75e9FC819D7",
+    verifier: "0x2168609301437822c7AD3f35114B10941866F20a",
+  },
 };
